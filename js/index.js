cf745b28354bf7b732af1bed164fbdf668a3ee4f
Matt <matthieu.dev.degny@gmail.com>
2022-04-24 20:32:40 +0200

    particles.js installed and half set

--- js/index.js
+++ js/index.js
@@ -106,3 +106,7 @@ const app = {
 
 // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
 document.addEventListener('DOMContentLoaded', app.init );
+
+
+
+    
