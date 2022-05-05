6356149ac8eb219ac43f69d974574e9766ccc938
Matt <matthieu.dev.degny@gmail.com>
2022-05-05 20:40:59 +0200

    object app2 done + fetch ok

--- js/appPage2.js
+++ js/appPage2.js
@@ -1,14 +1,9 @@
-
-// var myHeaders = new Headers();
-// var myInit = { method: 'GET',
-//                headers: myHeaders,
-//                mode: 'cors',
-//                cache: 'default' };
 const app2 = {
 
     objects:{
         result : document.getElementById("inputResearch"),
-        button : document.getElementById("button")
+        button : document.getElementById("button"),
+        containerPictures : document.getElementById("container-pictures")
     },
 
     init:function () {
@@ -16,24 +11,57 @@ const app2 = {
     },
 
     listener: function(){
-        button.addEventListener("click", async () => {
+        app2.objects.result.addEventListener("click", () => {
+            app2.objects.result.value = ""
+        })
+
+        app2.objects.button.addEventListener("click", () => {
+            app2.clearContainerPictures()
             let {result} = app2.objects
             result = result.value
             const API =`https://images-api.nasa.gov/search?q=${result}&media_type=image`
-            let response = await fetch ("https://images-api.nasa.gov/search?q=sun&media_type=image");
-            let data = await response.json();
-            if(data){
-                console.log(data);}
-            
-            else{
-                console.log("problem with request")} 
-
+            app2.requestAction2.sendApiRequestInput(API)
         })
     },
 
-   
+    requestAction2:{
+        sendApiRequestInput: async function (API) {
+            let response = await fetch (API);
+            let data = await response.json();
+            if(data){
+                console.log(data)
+                app2.useAPIdata2(data);}
+            
+            else{
+                console.log("problem with request")} 
+        }
+    },
 
+    useAPIdata2:function (data) {
+        for(let i = 0; i < 10; i++){
 
+            fetch(data.collection.items[i].href)
+            .then(res=>{
+                if(res.ok){
+                res.json().then(data=>{
+                    for(let image of data){ 
+                    if(image.includes("small")) {
+                        let newDiv = document.createElement("div");
+                        newDiv.classList.add("images")
+                        console.log(image)
+                        newDiv.innerHTML = `<img src="${image}" alt="">`
+                        document.getElementById("container-pictures").appendChild(newDiv)
+                    }
+                    }
+                })
+            }
+        })
+        }
+    },
+
+    clearContainerPictures: function() {
+        app2.objects.containerPictures.innerHTML = ""
+    }
 }
 
 // when the loading is finish i launch app.init
