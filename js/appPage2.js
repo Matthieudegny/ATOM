4cbbd4ebba8cf89a6539ce539d262ee3f9d7d73d
Matt <matthieu.dev.degny@gmail.com>
2022-05-05 19:55:16 +0200

    beginning creation of the app2 main Object

--- js/appPage2.js
+++ js/appPage2.js
@@ -1,57 +1,79 @@
-// "https://images-api.nasa.gov/search?q=pesquet&media_type=image"
+
 // var myHeaders = new Headers();
 // var myInit = { method: 'GET',
 //                headers: myHeaders,
 //                mode: 'cors',
 //                cache: 'default' };
-const app = {
+const app2 = {
 
     objects:{
-        
-    }
+        result : document.getElementById("inputResearch"),
+        button : document.getElementById("button")
+    },
 
+    init:function () {
+        app2.listener()
+    },
+
+    listener: function(){
+        button.addEventListener("click", async () => {
+            let {result} = app2.objects
+            result = result.value
+            const API =`https://images-api.nasa.gov/search?q=${result}&media_type=image`
+            let response = await fetch ("https://images-api.nasa.gov/search?q=sun&media_type=image");
+            let data = await response.json();
+            if(data){
+                console.log(data);}
+            
+            else{
+                console.log("problem with request")} 
+
+        })
+    },
+
+   
 
 
 }
-const result = document.getElementById("inputResearch")
-const button = document.getElementById("button")
 
-button.addEventListener("click", () => {
-   const researchToDo = result.value
-   const API =`https://images-api.nasa.gov/search?q=${researchToDo}&media_type=image`
+// when the loading is finish i launch app.init
+document.addEventListener('DOMContentLoaded', app2.init );
+
+// button.addEventListener("click", () => {
+//    const researchToDo = result.value
+//    const API =`https://images-api.nasa.gov/search?q=sun&media_type=image`
    
-   fetch(API)
+   
 // .then(res=>res.json())
 // .then(data => console.log(data))
 
-.then(res => {
-    if(res.ok){
-        res.json().then(data=> {
+// .then(res => {
+//     if(res.ok){
+//         res.json().then(data=> {
             
             
-            for(let i = 0; i < 10; i++){
+//             for(let i = 0; i < 10; i++){
 
-                fetch(data.collection.items[i].href)
-                .then(res=>{
-                    if(res.ok){
-                    res.json().then(data=>{
-                        for(let image of data){ //{console.log(image)}
-                        if(image.includes("small")) {
-                            let newDiv = document.createElement("div");
-                            newDiv.classList.add("images")
-                            newDiv.innerHTML = `<img src="${image}" alt="">`
-                            document.getElementById("body").appendChild(newDiv)
-                        }
-                        }
-                        console.log("espace")
-                    })
-                }
-            })
-            }
+//                 fetch(data.collection.items[i].href)
+//                 .then(res=>{
+//                     if(res.ok){
+//                     res.json().then(data=>{
+//                         for(let image of data){ //{console.log(image)}
+//                         if(image.includes("small")) {
+//                             let newDiv = document.createElement("div");
+//                             newDiv.classList.add("images")
+//                             newDiv.innerHTML = `<img src="${image}" alt="">`
+//                             document.getElementById("body").appendChild(newDiv)
+//                         }
+//                         }
+//                         console.log("espace")
+//                     })
+//                 }
+//             })
+//             }
 
             
            
-        })
-    }
-})
-})
+//         })
+//     }
+// })
