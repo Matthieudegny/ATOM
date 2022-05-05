41fc6f2dab4f9439f10c61eb40e2272afc450e65
Matt <matthieu.dev.degny@gmail.com>
2022-05-05 19:30:24 +0200

    input+button+fetch set -> done

--- js/appPage2.js
+++ js/appPage2.js
@@ -1,11 +1,26 @@
+// "https://images-api.nasa.gov/search?q=pesquet&media_type=image"
+// var myHeaders = new Headers();
+// var myInit = { method: 'GET',
+//                headers: myHeaders,
+//                mode: 'cors',
+//                cache: 'default' };
+const app = {
 
-var myHeaders = new Headers();
-var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };
+    objects:{
+        
+    }
 
-fetch("https://images-api.nasa.gov/search?q=pesquet&media_type=image",myInit)
+
+
+}
+const result = document.getElementById("inputResearch")
+const button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+   const researchToDo = result.value
+   const API =`https://images-api.nasa.gov/search?q=${researchToDo}&media_type=image`
+   
+   fetch(API)
 // .then(res=>res.json())
 // .then(data => console.log(data))
 
@@ -13,12 +28,12 @@ fetch("https://images-api.nasa.gov/search?q=pesquet&media_type=image",myInit)
     if(res.ok){
         res.json().then(data=> {
             
-
+            
             for(let i = 0; i < 10; i++){
 
                 fetch(data.collection.items[i].href)
                 .then(res=>{
-                if(res.ok){
+                    if(res.ok){
                     res.json().then(data=>{
                         for(let image of data){ //{console.log(image)}
                         if(image.includes("small")) {
@@ -38,4 +53,5 @@ fetch("https://images-api.nasa.gov/search?q=pesquet&media_type=image",myInit)
            
         })
     }
+})
 })
